93a2ad1a59038d55cedaee2284ff4e4243662751
Marta Luciani <88147394+marta-lu@users.noreply.github.com>
2021-10-11 17:42:48 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,16 @@ function update_carousel_words(){
     counter = 0;
     }
     }
+
+
+    const gra = function(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    const init = function(){
+      let items = document.querySelectorAll('section');
+      for (let i = 0; i < items.length; i++){
+        items[i].style.background = randomColor({luminosity: 'light'});
+      }
+      cssScrollSnapPolyfill()
+    }
+    init();
